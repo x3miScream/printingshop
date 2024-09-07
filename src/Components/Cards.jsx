19ec61d569662808a1732f5c3016eeac918508f6
ataibekenov <ataybekenov@gmail.com>
2024-09-07 16:15:48 +0800

    adding packaging picture

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -10,7 +10,6 @@ const Cards = (props) => {
     return(
         <div className='cards'>
             <div className='cards__container'>
-                {/* {cardsData.map((item, index) => {return <ul className='cards__items'> {item.map((item, subIndex) => <CardItem id={item.id} src={settings.hostingBaseUrl + item.imageSource} text={item.text} label={item.label} path={item.path}> </CardItem>)} </ul> })} */}
                 <ul className='cards__items'>
                     {cardsData.map((item, index) => { if(item.id !==currentCardId) return <CardItem key={item.id} id={item.id} src={settings.hostingBaseUrl + item.imageSource} text={item.text} label={item.label} path={settings.hostingBaseUrl + item.path}> </CardItem>}) }
                 </ul>
